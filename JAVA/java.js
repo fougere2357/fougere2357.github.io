3591d550cf36f1be9ab1530b239ff070952b4fef
Mani <mani.dehghan4@gmail.com>
2021-06-13 15:19:29 -0700

    tgwsb

---
+++ JAVA/java.js
@@ -0,0 +1,56 @@
+let fire= new Audio("sounds/fire-1.wav")
+
+function plays() {
+    fire.play();
+}
+
+let stop= new Audio("sounds/stop.wav")
+
+function stopp() {
+    stop.play()
+}
+
+let waters= new Audio("sounds/water.wav")
+
+function water() {
+    waters.play()
+}
+
+let need= new Audio("sounds/Need.wav")
+
+let needs= ()=>{
+    need.play()
+}
+
+let parade= new Audio("sounds/parade.wav")
+
+let does= ()=>[
+    parade.play()
+]
+
+let anthem= new Audio("sounds/anthem.wav");
+let sing= ()=>{
+    anthem.play()
+}
+
+
+let blahs= new Audio("sounds/Blah.wav")
+function blah() {
+    blahs.play()
+}
+
+
+let guitar= new Audio("sounds/guitar.wav")
+function gplay() {
+    guitar.play()
+}
+
+let plants= new Audio('sounds/plant.wav')
+function plant() {
+    plants.play()
+}
+
+let light= new Audio("sounds/light.wav")
+let lights= ()=>{
+    light.play()
+}
